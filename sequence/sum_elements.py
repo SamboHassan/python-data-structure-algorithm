99f457c544806c7443a2a706332310e197f312d9
SamboHassan <sambohasan@gmail.com>
2024-08-07 17:39:29 +0100

    add exercise files

---
+++ sequence/sum_elements.py
@@ -0,0 +1,40 @@
+def sum_element(S):
+    """Return the sum of elements in sequence S"""
+    n = len(S)
+    sum = 0
+    for e in range(n):
+        sum += S[e]
+    return sum
+
+
+def sum_of_even_element(S):
+    """Return sum of elements with even index"""
+
+    total = 0
+    for e in range(0, len(S), 2):
+        total += S[e]
+    return total
+
+
+def sum_of_prefix_sum(S):
+    n = len(S)
+    prefix = 0
+    total = 0
+    for j in range(n):
+        prefix += S[j]
+        total += prefix
+    return total
+
+
+from itertools import accumulate
+
+seq = [1, 2, 3, 4, 5]
+print(sum_of_prefix_sum(seq))
+print(sum(accumulate(seq)))
+print()
+# Prefix sum
+a = [1, 2, 3, 4, 5]
+b = [1, 3, 6, 10, 15]
+
+for n in accumulate(a):
+    print(n)
