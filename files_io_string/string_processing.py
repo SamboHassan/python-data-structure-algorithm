92de51341c2b871f4fb5e6f5ecef2b2c33d76082
SamboHassan <sambohasan@gmail.com>
2024-10-31 17:12:29 +0100

    add files

---
+++ files_io_string/string_processing.py
@@ -0,0 +1,8 @@
+text = 'spam = "%-5s", %+06d' % ("ham", 99)  # more complex formatting
+
+print(text)
+"A B C D".split()
+"--".join(["a", "b", "c"])
+from sys import *
+
+stdout.write(("." * 4).join(stdin.read().split("\t")))
