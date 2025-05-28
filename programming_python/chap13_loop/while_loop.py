d46216bce25293f07b87abd883292ad10f9c254f
SamboHassan <sambohasan@gmail.com>
2025-05-28 14:56:06 +0100

    update files

--- programming_python/chap13_loop/while_loop.py
+++ programming_python/chap13_loop/while_loop.py
@@ -1,48 +1,93 @@
 """
-The language’s two main looping constructs—statements that repeat an action over and
-over. The first of these, the while statement, provides a way to code general loops. 
+looping constructs are statements that repeat an action over and
+over.
+
+The language’s two main looping constructs are while and for loop
+
+The while statement, provides a way to code general loops.
+
 The second, the for statement, is designed for stepping through the items in a 
 sequence or other iterable object and running a block of code for each.
 
 """
 
-
-In simple terms, it repeatedly executes a block of (normally indented) statements as long
-as a test at the top keeps evaluating to a true value. It is called a “loop” because control
-keeps looping back to the start of the statement until the test becomes false. When the
-test becomes false, control passes to the statement that follows the while block. The
-net effect is that the loop’s body is executed repeatedly while the test at the top is true.
-If the test is false to begin with, the body never runs and the while statement is skipped.
+# It is called a “loop” because control keeps looping back to the start of the 
+# statement until the test becomes false.
 
 
-----------------------  General Format
+# ----------------------  General Format
 
-while test: # Loop test
- 	statements # Loop body
-else: # Optional else
- 	statements # Run if didn't exit loop with break
-
-
->>> while True:
-... print('Type Ctrl-C to stop me!')
+while test: 		# Loop test
+ 	statements 		# Loop body
+else: 				# Optional else
+ 	statements 		# Run if didn't exit loop with break
 
 
->>> x = 'spam'
->>> while x: # While x is not empty
-... print(x, end=' ') # In 2.X use print x,
-... x = x[1:] # Strip first character off x
-...
+
+
+while True: 
+	print('Type Ctrl-C to stop me!')
+
+
+x = 'spam'
+while x: 				# While x is not empty
+	print(x, end=' ') 	# In 2.X use print x,
+	x = x[1:] 			# Strip first character off x
+
 spam pam am m
 
 
 # The following code counts from the value of a up to, but not including, b.
->>> a=0; b=10
->>> while a < b: # One way to code counter loops
-... print(a, end=' ')
-... a += 1 # Or, a = a + 1
+
+a=0; b=10
+while a < b: 			# One way to code counter loops
+	print(a, end=' ')
+	a += 1 				# Or, a = a + 1
+
+# ---------—---the break and continue statements.
+
+#These are the two simple statements that have a purpose only when nested 
+# inside loops
+
+"""
+---break
+Jumps out of the closest enclosing loop (past the entire loop statement)
+
+---continue
+Jumps to the top of the closest enclosing loop (to the loop’s header line)
+
+---pass
+Does nothing at all: it’s an empty statement placeholder
+
+---Loop else block
+Runs if and only if the loop is exited normally (i.e., without hitting a break)
+
+"""
+
+
+# -------- General Loop Format
+# Factoring in break and continue statements, the general format of the while 
+# loop looks like this:
+
+while test:
+	statements
+	if test: break 			# Exit loop now, skip else if present
+ 	if test: continue 		# Go to top of loop now, to test1
+else:
+ 	statements 				# Run if we didn't hit a 'break'
+
+
+# continue  -- The continue statement causes an immediate jump to the top 
+# of a loop.
+# You can also sometimes avoid nesting by including a break or continue
 
 
 
+while True:
+	name = input('Enter name:') 
+	if name == 'stop': break
+	age = input('Enter age: ')
+	print('Hello', name, '=>', int(age) ** 2)
 
 
 
--- programming_python/summer2.py
+++ programming_python/summer2.py
@@ -1,17 +1,28 @@
 def summer(numCols, fileName):
- sums = [0] * numCols
- for line in open(fileName): # use file iterators
- cols = line.split(',') # assume comma-delimited
- nums = [int(x) for x in cols] # use limited converter
- both = zip(sums, nums) # avoid nested for loop
- sums = [x + y for (x, y) in both] # 3.X: zip is an iterable
- return sums
+    sums = [0] * numCols
+    for line in open(fileName):
+        cols = line.split(",")
+        nums = [int(x) for x in cols]
+        both = zip(sums, nums)
+        sums = [x + y for (x, y) in both]
+    return sums
 
 
-C:\...\PP4E\Lang> type table3.txt
-1,5,10,2,1
-2,10,20,4,2
-3,15,30,8,3
-4,20,40,16,4
-C:\...\PP4E\Lang> python summer2.py 5 table3.txt
-[10, 50, 100, 30, 10]
+# def summer(numCols, fileName):
+# 	sums = [0] * numCols
+# 	for line in open(fileName):
+# 		# use file iterators
+# 		 cols = line.split(',') # assume comma-delimited
+# 		 nums = [int(x) for x in cols] # use limited converter
+# 		 both = zip(sums, nums) # avoid nested for loop
+# 		 sums = [x + y for (x, y) in both] # 3.X: zip is an iterable
+# 	 	return sums
+
+
+# C:\...\PP4E\Lang> type table3.txt
+# 1,5,10,2,1
+# 2,10,20,4,2
+# 3,15,30,8,3
+# 4,20,40,16,4
+# C:\...\PP4E\Lang> python summer2.py 5 table3.txt
+# [10, 50, 100, 30, 10]
